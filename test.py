5fb083e43dee05e3af740bb938f52001a4066a6b
belajdel <belajdelmedamine@gmail.com>
2023-07-26 08:17:57 +0100

    reversed_some_of_the_main_functions

---
+++ test.py
@@ -0,0 +1,7 @@
+"""import marshal
+import base64
+print(base64.b32decode("MZZG63JAO5QXE3TBEBUW24DPOJ2CA4DFNRQW4Z3JBIFHAZLMMFXGO2JIEJYGYZLBONSSA53BNF2CELBXFQZCSIBDMFZGO4Z5FA6HO33SMRZT4LB4ORUW2ZJ6FQ6HIYLCHYUQU==="))"""
+from warna import pelangi
+
+pelangi("please wait", 7, 2) #args=(<words>,<time>,<tab>)
+
